019773d929334ef15fea4d3db9dbe43c2cb1cdc6
kaitoulee <hackerheilian@vip.qq.com>
2016-04-22 21:22:13 +0800

    自定义协议
    这个会启动 url-mail-tel-sms这个应用.这里里面重写了url自定义协议

---
+++ customProtocol/LabelViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LabelViewController.swift
+//  customProtocol
+//
+//  Created by kaitouLee on 16/4/22.
+//  Copyright © 2016年 kaitou Lee. All rights reserved.
+//
+
+import UIKit
+
+class LabelViewController: UIViewController {
+    @IBOutlet weak var myLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
